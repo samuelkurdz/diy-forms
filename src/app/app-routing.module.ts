d9f860955769a57059d397c9ddadf27ca3b9ce8a
Samuel Adebayo <samuel.adebayo@engagetech.com>
2022-07-22 13:06:39 +0100

    add stand alone component for landing page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "",
+    loadComponent: () => import('./landing-page/landing-page.component')
+    .then(m => m.LandingPageComponent,)
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
